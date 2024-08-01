67b349b787e4b646966d55d87735418b9681cfbe
Abdullah-142 <abdulah14200@gmail.com>
2024-08-01 07:24:27 +0500

    delete add route

--- src/index.ts
+++ src/index.ts
@@ -28,19 +28,6 @@ app.get("/:id", async (req, res) => {
 	});
 });
 
-app.post("/add", async (req, res) => {
-	await client.user.create({
-		data: {
-			email: req.body.email,
-			name: req.body.name,
-		},
-	});
-
-	res.json({
-		message: "Done signing up!",
-	});
-});
-
 app.listen(3000, () => {
 	console.log("Server is running on port 3000");
 });
